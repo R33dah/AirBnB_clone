024e53e5d4c8f6af197401f66f96d7b8fd2c806c
r33edah <little.musahh@gmail.com>
2022-08-15 20:34:55 +0100

    AirBnB_clone

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+'Module for State'
+
+
+class State(BaseModel):
+    """ State class """
+
+    name = ""

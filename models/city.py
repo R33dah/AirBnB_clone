781e8cfd1dc915582cc8fceb411756b72e456305
r33edah <little.musahh@gmail.com>
2022-08-15 20:32:21 +0100

    AirBnB_clone

---
+++ models/city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+0x00. AirBnB clone - The console
+City module
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    Defines attributes/methods for the City class, subclass of BaseModel
+    Other attributes/methods are inherited from BaseModel
+    """
+
+    state_id = ""
+    name = ""
